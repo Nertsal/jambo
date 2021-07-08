edf1ee98bc9570f02d4635f8bc7ac5e6084862ba
Alexander <sasha.kudasov04@gmail.com>
2021-07-08 12:59:09 +0500

    Split timer into module

--- src/timer_bot/mod.rs
+++ src/timer_bot/mod.rs
@@ -1,78 +1,9 @@
 use super::*;
 
 mod commands;
+mod timer;
 
-struct Timer {
-    time: std::time::Duration,
-    paused: bool,
-    timer_mode: TimerMode,
-}
-
-#[derive(Clone, Copy)]
-enum TimerMode {
-    Idle,
-    Countdown,
-    Countup,
-}
-
-impl Timer {
-    fn from_status() -> Result<Self, Box<dyn std::error::Error>> {
-        let time = humantime::parse_duration(&std::fs::read_to_string(format!(
-            "status/{}.txt",
-            TimerBot::name()
-        ))?)?;
-        Ok(Self {
-            time,
-            paused: true,
-            timer_mode: TimerMode::Idle,
-        })
-    }
-
-    fn new_str(time: std::time::Duration, mode: &str) -> Result<Self, ()> {
-        let (paused, timer_mode) = match mode {
-            "set" => (true, TimerMode::Idle),
-            "countdown" => (false, TimerMode::Countdown),
-            "countup" => (false, TimerMode::Countup),
-            _ => {
-                return Err(());
-            }
-        };
-        Ok(Self {
-            time,
-            paused,
-            timer_mode,
-        })
-    }
-
-    fn update(&mut self, delta_time: f32) {
-        if !self.paused {
-            let delta = std::time::Duration::from_secs_f32(delta_time);
-            match self.timer_mode {
-                TimerMode::Idle => (),
-                TimerMode::Countdown => {
-                    self.time = self.time.checked_sub(delta).unwrap_or_default();
-                }
-                TimerMode::Countup => {
-                    self.time = self.time.checked_add(delta).unwrap_or_default();
-                }
-            }
-        }
-    }
-
-    fn time_status(&self) -> String {
-        humantime::format_duration(self.time).to_string()
-    }
-}
-
-impl Default for Timer {
-    fn default() -> Self {
-        Self {
-            paused: true,
-            time: std::time::Duration::from_secs(0),
-            timer_mode: TimerMode::Idle,
-        }
-    }
-}
+use timer::*;
 
 pub struct TimerBot {
     channel_login: String,
---
+++ src/timer_bot/timer.rs
@@ -0,0 +1,73 @@
+use super::*;
+
+pub struct Timer {
+    pub time: std::time::Duration,
+    pub paused: bool,
+    pub timer_mode: TimerMode,
+}
+
+#[derive(Clone, Copy)]
+pub enum TimerMode {
+    Idle,
+    Countdown,
+    Countup,
+}
+
+impl Timer {
+    pub fn from_status() -> Result<Self, Box<dyn std::error::Error>> {
+        let time = humantime::parse_duration(&std::fs::read_to_string(format!(
+            "status/{}.txt",
+            TimerBot::name()
+        ))?)?;
+        Ok(Self {
+            time,
+            paused: true,
+            timer_mode: TimerMode::Idle,
+        })
+    }
+
+    pub fn new_str(time: std::time::Duration, mode: &str) -> Result<Self, ()> {
+        let (paused, timer_mode) = match mode {
+            "set" => (true, TimerMode::Idle),
+            "countdown" => (false, TimerMode::Countdown),
+            "countup" => (false, TimerMode::Countup),
+            _ => {
+                return Err(());
+            }
+        };
+        Ok(Self {
+            time,
+            paused,
+            timer_mode,
+        })
+    }
+
+    pub fn update(&mut self, delta_time: f32) {
+        if !self.paused {
+            let delta = std::time::Duration::from_secs_f32(delta_time);
+            match self.timer_mode {
+                TimerMode::Idle => (),
+                TimerMode::Countdown => {
+                    self.time = self.time.checked_sub(delta).unwrap_or_default();
+                }
+                TimerMode::Countup => {
+                    self.time = self.time.checked_add(delta).unwrap_or_default();
+                }
+            }
+        }
+    }
+
+    pub fn time_status(&self) -> String {
+        humantime::format_duration(self.time).to_string()
+    }
+}
+
+impl Default for Timer {
+    fn default() -> Self {
+        Self {
+            paused: true,
+            time: std::time::Duration::from_secs(0),
+            timer_mode: TimerMode::Idle,
+        }
+    }
+}
