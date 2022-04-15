c40de9b08f7b25fbaee713ac7926a9c1d1329b14
Nertsal <sasha.kudasov04@gmail.com>
2022-04-15 22:25:30 +0300

    Reorder types

--- src/main_bot/mod.rs
+++ src/main_bot/mod.rs
@@ -19,138 +19,13 @@ fn constructors() -> impl IntoIterator<Item = (BotName, BotConstructor)> {
 
 // -- End of the section, do not modify anything below --
 
-impl linefeed::Completer<linefeed::DefaultTerminal> for MutexBot {
-    fn complete(
-        &self,
-        word: &str,
-        prompter: &Prompter,
-        start: usize,
-        end: usize,
-    ) -> Option<Vec<linefeed::Completion>> {
-        let mut main = futures::executor::block_on(self.0.lock());
-        let main_completetion = main.commands.complete(word, prompter, start, end);
-        let bots = &mut main.bots;
-
-        // To include the sub-bot into auto-completion, add a line in the match below
-        let mut completions = vec![main_completetion];
-        completions.extend(
-            bots.active
-                .values()
-                .map(|bot| bot.complete(word, prompter, start, end)),
-        );
-
-        Some(completions.into_iter().flatten().flatten().collect())
-    }
-}
-
-#[async_trait]
-pub trait Bot: Send {
-    async fn handle_message(
-        &mut self,
-        client: &TwitchClient,
-        channel: &ChannelLogin,
-        message: &CommandMessage,
-    );
-
-    fn complete(
-        &self,
-        word: &str,
-        prompter: &Prompter,
-        start: usize,
-        end: usize,
-    ) -> Option<Vec<linefeed::Completion>>;
-}
-
 pub struct MutexBot(Mutex<MainBot>);
-
-impl MutexBot {
-    pub fn new(bot: MainBot) -> Self {
-        Self(Mutex::new(bot))
-    }
-}
-
-impl std::ops::Deref for MutexBot {
-    type Target = Mutex<MainBot>;
-
-    fn deref(&self) -> &Self::Target {
-        &self.0
-    }
-}
-
-type BotConstructor = fn(&Cli) -> Box<dyn Bot>;
-
-struct Bots {
-    constructors: HashMap<BotName, BotConstructor>,
-    active: HashMap<BotName, Box<dyn Bot>>,
-}
-
-impl Bots {
-    fn new(cli: &Cli, active_bots: ActiveBots) -> Self {
-        let constructors = constructors().into_iter().collect::<HashMap<_, _>>();
-        let mut active = HashMap::new();
-        for bot_name in active_bots {
-            match constructors.get(&bot_name) {
-                Some(constructor) => {
-                    let bot = constructor(cli);
-                    log(cli, LogType::Info, &format!("Spawned {bot_name}"));
-                    active.insert(bot_name, bot);
-                }
-                None => {
-                    log(
-                        cli,
-                        LogType::Warn,
-                        &format!("Failed to find a constructor for {bot_name}"),
-                    );
-                }
-            }
-        }
-        Self {
-            constructors,
-            active,
-        }
-    }
-}
 
 pub struct MainBot {
     cli: Cli,
     commands: Commands<MainBot>,
     bots: Bots,
     pub queue_shutdown: bool,
-}
-
-impl BotPerformer for MainBot {
-    const NAME: &'static str = "MainBot";
-
-    fn commands(&self) -> &Commands<Self> {
-        &self.commands
-    }
-}
-
-#[async_trait]
-impl Bot for MainBot {
-    async fn handle_message(
-        &mut self,
-        client: &TwitchClient,
-        channel: &ChannelLogin,
-        message: &CommandMessage,
-    ) {
-        self.perform(&self.cli.clone(), client, channel, message)
-            .await;
-
-        for bot in self.bots.active.values_mut() {
-            bot.handle_message(client, channel, message).await;
-        }
-    }
-
-    fn complete(
-        &self,
-        word: &str,
-        prompter: &Prompter,
-        start: usize,
-        end: usize,
-    ) -> Option<Vec<linefeed::Completion>> {
-        self.commands.complete(word, prompter, start, end)
-    }
 }
 
 impl MainBot {
@@ -216,6 +91,107 @@ impl MainBot {
 }
 
 #[async_trait]
+pub trait Bot: Send {
+    async fn handle_message(
+        &mut self,
+        client: &TwitchClient,
+        channel: &ChannelLogin,
+        message: &CommandMessage,
+    );
+
+    fn complete(
+        &self,
+        word: &str,
+        prompter: &Prompter,
+        start: usize,
+        end: usize,
+    ) -> Option<Vec<linefeed::Completion>>;
+}
+
+impl MutexBot {
+    pub fn new(bot: MainBot) -> Self {
+        Self(Mutex::new(bot))
+    }
+}
+
+impl std::ops::Deref for MutexBot {
+    type Target = Mutex<MainBot>;
+
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
+
+type BotConstructor = fn(&Cli) -> Box<dyn Bot>;
+
+struct Bots {
+    constructors: HashMap<BotName, BotConstructor>,
+    active: HashMap<BotName, Box<dyn Bot>>,
+}
+
+impl Bots {
+    fn new(cli: &Cli, active_bots: ActiveBots) -> Self {
+        let constructors = constructors().into_iter().collect::<HashMap<_, _>>();
+        let mut active = HashMap::new();
+        for bot_name in active_bots {
+            match constructors.get(&bot_name) {
+                Some(constructor) => {
+                    let bot = constructor(cli);
+                    log(cli, LogType::Info, &format!("Spawned {bot_name}"));
+                    active.insert(bot_name, bot);
+                }
+                None => {
+                    log(
+                        cli,
+                        LogType::Warn,
+                        &format!("Failed to find a constructor for {bot_name}"),
+                    );
+                }
+            }
+        }
+        Self {
+            constructors,
+            active,
+        }
+    }
+}
+
+impl BotPerformer for MainBot {
+    const NAME: &'static str = "MainBot";
+
+    fn commands(&self) -> &Commands<Self> {
+        &self.commands
+    }
+}
+
+#[async_trait]
+impl Bot for MainBot {
+    async fn handle_message(
+        &mut self,
+        client: &TwitchClient,
+        channel: &ChannelLogin,
+        message: &CommandMessage,
+    ) {
+        self.perform(&self.cli.clone(), client, channel, message)
+            .await;
+
+        for bot in self.bots.active.values_mut() {
+            bot.handle_message(client, channel, message).await;
+        }
+    }
+
+    fn complete(
+        &self,
+        word: &str,
+        prompter: &Prompter,
+        start: usize,
+        end: usize,
+    ) -> Option<Vec<linefeed::Completion>> {
+        self.commands.complete(word, prompter, start, end)
+    }
+}
+
+#[async_trait]
 pub trait BotPerformer: Bot {
     const NAME: &'static str;
 
@@ -249,5 +225,29 @@ pub trait BotPerformer: Bot {
     fn update_status(&self, status_text: &str) {
         let path = format!("status/{}.txt", Self::NAME);
         std::fs::write(path, status_text).expect("Could not update bot status");
+    }
+}
+
+impl linefeed::Completer<linefeed::DefaultTerminal> for MutexBot {
+    fn complete(
+        &self,
+        word: &str,
+        prompter: &Prompter,
+        start: usize,
+        end: usize,
+    ) -> Option<Vec<linefeed::Completion>> {
+        let mut main = futures::executor::block_on(self.0.lock());
+        let main_completetion = main.commands.complete(word, prompter, start, end);
+        let bots = &mut main.bots;
+
+        // To include the sub-bot into auto-completion, add a line in the match below
+        let mut completions = vec![main_completetion];
+        completions.extend(
+            bots.active
+                .values()
+                .map(|bot| bot.complete(word, prompter, start, end)),
+        );
+
+        Some(completions.into_iter().flatten().flatten().collect())
     }
 }
