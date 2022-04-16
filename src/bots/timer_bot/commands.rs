267403360f260ca48092d2aaddd728c9fc1baa7a
Nertsal <sasha.kudasov04@gmail.com>
2022-04-16 20:51:58 +0300

    Improve TimerBot

--- src/bots/timer_bot/commands.rs
+++ src/bots/timer_bot/commands.rs
@@ -3,6 +3,23 @@ use std::sync::Arc;
 use super::*;
 
 impl TimerBot {
+    fn timer_from_str(&mut self, mode: &str, time: Option<&str>) -> Response {
+        let time = match time {
+            Some(time) => match Timer::parse_duration(time) {
+                Ok(time) => Some(time),
+                Err(err) => return Some(format!("Failed to parse time argument: {err}")),
+            },
+            None => None,
+        };
+
+        match mode {
+            "set" => self.timer_set(TimerMode::Idle, time),
+            "countup" => self.timer_set(TimerMode::Countup, time),
+            "countdown" => self.timer_set(TimerMode::Countdown, time),
+            _ => return None,
+        }
+    }
+
     fn timer_pause(&mut self, paused: bool) -> Response {
         self.timer.paused = paused;
         match paused {
@@ -48,28 +65,13 @@ impl TimerBot {
         let set_time = CommandBuilder::<Self, _>::new().word().finalize(
             true,
             AuthorityLevel::Moderator as _,
-            Arc::new(
-                |bot, _, args| match Timer::parse_duration(args[1].as_str()) {
-                    Ok(time) => match args[0].as_str() {
-                        "set" => bot.timer_set(TimerMode::Idle, Some(time)),
-                        "countup" => bot.timer_set(TimerMode::Countup, Some(time)),
-                        "countdown" => bot.timer_set(TimerMode::Countdown, Some(time)),
-                        _ => return None,
-                    },
-                    Err(err) => Some(format!("Failed to parse time argument: {err}")),
-                },
-            ),
+            Arc::new(|bot, _, args| bot.timer_from_str(&args[0], Some(&args[1]))),
         );
 
         let set_no_time = CommandBuilder::<Self, _>::new().finalize(
             true,
             AuthorityLevel::Moderator as _,
-            Arc::new(|bot, _, args| match args[0].as_str() {
-                "set" => bot.timer_set(TimerMode::Idle, None),
-                "countup" => bot.timer_set(TimerMode::Countup, None),
-                "countdown" => bot.timer_set(TimerMode::Countdown, None),
-                _ => return None,
-            }),
+            Arc::new(|bot, _, args| bot.timer_from_str(&args[0], None)),
         );
 
         let set = CommandBuilder::new()
