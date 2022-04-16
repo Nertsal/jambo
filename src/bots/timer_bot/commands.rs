fea16a9284d1fcb68d388dfebb8fa5749eed1905
Nertsal <sasha.kudasov04@gmail.com>
2022-04-16 20:39:35 +0300

    Use builder for TimerBot

--- src/bots/timer_bot/commands.rs
+++ src/bots/timer_bot/commands.rs
@@ -31,65 +31,55 @@ impl TimerBot {
 
     pub fn commands() -> Commands<Self> {
         Commands {
-            commands: vec![CommandNode::literal(
-                ["!timer"],
-                vec![
-                    CommandNode::argument_choice(
-                        ["pause", "continue"],
-                        vec![CommandNode::final_node(
+            commands: vec![CommandBuilder::new().literal(["!timer"]).split([
+                CommandBuilder::<Self, _>::new()
+                    .choice(["pause", "continue"])
+                    .finalize(
+                        true,
+                        AuthorityLevel::Broadcaster as _,
+                        Arc::new(|bot, _, args| {
+                            let paused = match args[0].as_str() {
+                                "pause" => true,
+                                "continue" => false,
+                                _ => return None,
+                            };
+                            bot.timer_pause(paused)
+                        }),
+                    ),
+                CommandBuilder::new()
+                    .choice(["set", "countup", "countdown"])
+                    .split([
+                        CommandBuilder::<Self, _>::new().word().finalize(
                             true,
-                            AuthorityLevel::Broadcaster as _,
+                            AuthorityLevel::Moderator as _,
                             Arc::new(|bot, _, args| {
-                                let paused = match args[0].as_str() {
-                                    "pause" => true,
-                                    "continue" => false,
-                                    _ => return None,
-                                };
-                                bot.timer_pause(paused)
-                            }),
-                        )],
-                    ),
-                    CommandNode::argument_choice(
-                        ["set", "countup", "countdown"],
-                        vec![
-                            CommandNode::argument(
-                                ArgumentType::Word,
-                                vec![CommandNode::final_node(
-                                    true,
-                                    AuthorityLevel::Moderator as _,
-                                    Arc::new(|bot, _, args| {
-                                        match Timer::parse_duration(args[1].as_str()) {
-                                            Ok(time) => match args[0].as_str() {
-                                                "set" => bot.timer_set(TimerMode::Idle, Some(time)),
-                                                "countup" => {
-                                                    bot.timer_set(TimerMode::Countup, Some(time))
-                                                }
-                                                "countdown" => {
-                                                    bot.timer_set(TimerMode::Countdown, Some(time))
-                                                }
-                                                _ => return None,
-                                            },
-                                            Err(err) => Some(format!(
-                                                "Failed to parse time argument: {err}"
-                                            )),
+                                match Timer::parse_duration(args[1].as_str()) {
+                                    Ok(time) => match args[0].as_str() {
+                                        "set" => bot.timer_set(TimerMode::Idle, Some(time)),
+                                        "countup" => bot.timer_set(TimerMode::Countup, Some(time)),
+                                        "countdown" => {
+                                            bot.timer_set(TimerMode::Countdown, Some(time))
                                         }
-                                    }),
-                                )],
-                            ),
-                            CommandNode::final_node(
-                                true,
-                                AuthorityLevel::Moderator as _,
-                                Arc::new(|bot, _, args| match args[0].as_str() {
-                                    "set" => bot.timer_set(TimerMode::Idle, None),
-                                    "countup" => bot.timer_set(TimerMode::Countup, None),
-                                    "countdown" => bot.timer_set(TimerMode::Countdown, None),
-                                    _ => return None,
-                                }),
-                            ),
-                        ],
-                    ),
-                ],
-            )],
+                                        _ => return None,
+                                    },
+                                    Err(err) => {
+                                        Some(format!("Failed to parse time argument: {err}"))
+                                    }
+                                }
+                            }),
+                        ),
+                        CommandBuilder::new().finalize(
+                            true,
+                            AuthorityLevel::Moderator as _,
+                            Arc::new(|bot, _, args| match args[0].as_str() {
+                                "set" => bot.timer_set(TimerMode::Idle, None),
+                                "countup" => bot.timer_set(TimerMode::Countup, None),
+                                "countdown" => bot.timer_set(TimerMode::Countdown, None),
+                                _ => return None,
+                            }),
+                        ),
+                    ]),
+            ])],
         }
     }
 }
