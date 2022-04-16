b06b0b01c3ddf8d055c98bf3ee828a8d03cc6438
Nertsal <sasha.kudasov04@gmail.com>
2022-04-16 21:29:24 +0300

    Improve QuoteBot

--- src/bots/quote_bot/commands.rs
+++ src/bots/quote_bot/commands.rs
@@ -3,192 +3,149 @@ use rand::seq::SliceRandom;
 use super::*;
 
 impl QuoteBot {
+    fn quote_random(&self) -> Response {
+        if let Some(random_quote_name) = self
+            .config
+            .quotes
+            .keys()
+            .collect::<Vec<&String>>()
+            .choose(&mut rand::thread_rng())
+        {
+            Some(format!(
+                "Quote {}: {}",
+                random_quote_name, self.config.quotes[random_quote_name as &str]
+            ))
+        } else {
+            Some(format!(
+                "No quotes yet. Add new ones with !quote add <quote>"
+            ))
+        }
+    }
+
+    fn quote_new(&mut self, quote_name: String, quote: String) -> Response {
+        if self.config.quotes.contains_key(&quote_name) {
+            Some(format!(
+                "A quote with the name {} already exists",
+                quote_name
+            ))
+        } else {
+            let response = Some(format!("Added new quote {}: {}", quote_name, quote));
+            self.config.quotes.insert(quote_name, quote);
+            self.config.save().unwrap();
+            response
+        }
+    }
+
+    fn quote_remove(&mut self, quote_name: &str) -> Response {
+        match self.config.quotes.remove(quote_name) {
+            Some(quote) => {
+                self.config.save().unwrap();
+                Some(format!("Deleted quote {:?}: {}", quote_name, quote))
+            }
+            None => Some(format!("I don't know any quote named {quote_name}. Try creating one with !quote new <quote_name> <quote>")),
+        }
+    }
+
+    fn quote_edit(&mut self, quote_name: &str, new_quote: String) -> Response {
+        match self.config.quotes.get_mut(quote_name) {
+            Some(old_quote) => {
+                let response = Some(format!(
+                    "Edited quote {}: {}. New quote: {}",
+                    quote_name, old_quote, new_quote
+                ));
+                *old_quote = new_quote;
+                self.config.save().unwrap();
+                response
+            }
+            None => {
+                Some(format!("I don't know any quote named {quote_name}. Try creating one with !quote new <quote_name> <quote>"))
+            }
+        }
+    }
+
+    fn quote_get(&mut self, quote_name: &str) -> Response {
+        match self.config.quotes.get(quote_name) {
+            Some(quote) => {
+                Some(quote.clone())
+            }
+            None => {
+                Some(format!("I don't know any quote named {quote_name}. Try creating one with !quote new <quote_name> <quote>"))
+            },
+        }
+    }
+
+    fn quote_rename(&mut self, quote_name: &str, new_name: String) -> Response {
+        if self.config.quotes.contains_key(&new_name) {
+            Some(format!("A quote with name {} already exists", new_name))
+        } else if let Some(quote) = self.config.quotes.remove(quote_name) {
+            let response = Some(format!(
+                "Changed quote's name from {} to {}",
+                quote_name, new_name
+            ));
+            self.config.quotes.insert(new_name, quote);
+            self.config.save().unwrap();
+            response
+        } else {
+            Some(format!("No quote with name {} found", quote_name))
+        }
+    }
+
     pub fn commands() -> Commands<Self> {
+        let random = CommandBuilder::<Self, _>::new().finalize(
+            true,
+            AuthorityLevel::Viewer as _,
+            Arc::new(|bot, _, _| bot.quote_random()),
+        );
+
+        let new = CommandBuilder::<Self, _>::new()
+            .literal(["new", "add"])
+            .word()
+            .line()
+            .finalize(
+                true,
+                AuthorityLevel::Moderator as _,
+                Arc::new(|bot, _, args| bot.quote_new(args[0].to_owned(), args[1].to_owned())),
+            );
+
+        let remove = CommandBuilder::<Self, _>::new()
+            .literal(["delete", "remove"])
+            .word()
+            .finalize(
+                true,
+                AuthorityLevel::Moderator as _,
+                Arc::new(|bot, _, args| bot.quote_remove(&args[0])),
+            );
+
+        let edit = CommandBuilder::<Self, _>::new()
+            .literal(["edit"])
+            .word()
+            .line()
+            .finalize(
+                true,
+                AuthorityLevel::Moderator as _,
+                Arc::new(|bot, _, args| bot.quote_edit(&args[0], args[1].to_owned())),
+            );
+
+        let rename = CommandBuilder::<Self, _>::new()
+            .literal(["rename"])
+            .word()
+            .word()
+            .finalize(
+                true,
+                AuthorityLevel::Moderator as _,
+                Arc::new(|bot, _, args| bot.quote_rename(&args[0], args[1].to_owned())),
+            );
+
+        let get = CommandBuilder::<Self, _>::new().word().line().finalize(
+            true,
+            AuthorityLevel::Viewer as _,
+            Arc::new(|bot, _, args| bot.quote_get(&args[0])),
+        );
+
         Commands {
-            commands: vec![CommandNode::Literal {
-                literals: vec!["!quote".to_owned()],
-                child_nodes: vec![
-                    CommandNode::final_node(
-                        true,
-                        AuthorityLevel::Viewer as usize,
-                        Arc::new(|bot, _, _| {
-                            if let Some(random_quote_name) = bot
-                                .config
-                                .quotes
-                                .keys()
-                                .collect::<Vec<&String>>()
-                                .choose(&mut rand::thread_rng())
-                            {
-                                Some(format!(
-                                    "Quote {}: {}",
-                                    random_quote_name, bot.config.quotes[random_quote_name as &str]
-                                ))
-                            } else {
-                                Some(format!(
-                                    "No quotes yet. Add new ones with !quote add <quote>"
-                                ))
-                            }
-                        }),
-                    ),
-                    CommandNode::Literal {
-                        literals: vec!["add".to_owned()],
-                        child_nodes: vec![CommandNode::Argument {
-                            argument_type: ArgumentType::Word,
-                            child_nodes: vec![CommandNode::Argument {
-                                argument_type: ArgumentType::Line,
-                                child_nodes: vec![CommandNode::final_node(
-                                    true,
-                                    AuthorityLevel::Moderator as usize,
-                                    Arc::new(|bot, _, args| {
-                                        if let [quote_name, quote] = args.as_slice() {
-                                            let response =
-                                                if bot.config.quotes.contains_key(quote_name) {
-                                                    Some(format!(
-                                                        "A quote with the name {} already exists",
-                                                        quote_name
-                                                    ))
-                                                } else {
-                                                    let response = Some(format!(
-                                                        "Added new quote {}: {}",
-                                                        quote_name, quote
-                                                    ));
-                                                    bot.config.quotes.insert(
-                                                        quote_name.to_owned(),
-                                                        quote.to_owned(),
-                                                    );
-                                                    bot.config.save().unwrap();
-                                                    response
-                                                };
-                                            return response;
-                                        }
-                                        None
-                                    }),
-                                )],
-                            }],
-                        }],
-                    },
-                    CommandNode::Literal {
-                        literals: vec!["delete".to_owned()],
-                        child_nodes: vec![CommandNode::Argument {
-                            argument_type: ArgumentType::Word,
-                            child_nodes: vec![CommandNode::final_node(
-                                true,
-                                AuthorityLevel::Moderator as usize,
-                                Arc::new(|bot, _, mut args| {
-                                    let quote_name = args.remove(0);
-                                    if let Some(quote) = bot.config.quotes.remove(&quote_name) {
-                                        let response = Some(format!(
-                                            "Deleted quote {:?}: {}",
-                                            quote_name, quote
-                                        ));
-                                        bot.config.save().unwrap();
-                                        return response;
-                                    }
-                                    None
-                                }),
-                            )],
-                        }],
-                    },
-                    CommandNode::Literal {
-                        literals: vec!["edit".to_owned()],
-                        child_nodes: vec![CommandNode::Argument {
-                            argument_type: ArgumentType::Word,
-                            child_nodes: vec![CommandNode::Argument {
-                                argument_type: ArgumentType::Line,
-                                child_nodes: vec![CommandNode::final_node(
-                                    true,
-                                    AuthorityLevel::Moderator as usize,
-                                    Arc::new(|bot, _, args| {
-                                        if let [quote_name, quote] = args.as_slice() {
-                                            let response = if let Some(old_quote) =
-                                                bot.config.quotes.get_mut(quote_name)
-                                            {
-                                                let response = Some(format!(
-                                                    "Edited quote {}: {}. New quote: {}",
-                                                    quote_name, old_quote, quote
-                                                ));
-                                                *old_quote = quote.to_owned();
-                                                response
-                                            } else {
-                                                let response = Some(format!(
-                                                    "Added new quote {}: {}",
-                                                    quote_name, quote
-                                                ));
-                                                bot.config.quotes.insert(
-                                                    quote_name.to_owned(),
-                                                    quote.to_owned(),
-                                                );
-                                                response
-                                            };
-                                            bot.config.save().unwrap();
-                                            return response;
-                                        }
-                                        None
-                                    }),
-                                )],
-                            }],
-                        }],
-                    },
-                    CommandNode::Literal {
-                        literals: vec!["rename".to_owned()],
-                        child_nodes: vec![CommandNode::Argument {
-                            argument_type: ArgumentType::Word,
-                            child_nodes: vec![CommandNode::Argument {
-                                argument_type: ArgumentType::Word,
-                                child_nodes: vec![CommandNode::final_node(
-                                    true,
-                                    AuthorityLevel::Moderator as usize,
-                                    Arc::new(|bot, _, args| {
-                                        if let [quote_name, quote_new_name] = args.as_slice() {
-                                            let response =
-                                                if bot.config.quotes.contains_key(quote_new_name) {
-                                                    Some(format!(
-                                                        "A quote with name {} already exists",
-                                                        quote_new_name
-                                                    ))
-                                                } else if let Some(quote) =
-                                                    bot.config.quotes.remove(quote_name)
-                                                {
-                                                    let response = Some(format!(
-                                                        "Changed quote's name from {} to {}",
-                                                        quote_name, quote_new_name
-                                                    ));
-                                                    bot.config
-                                                        .quotes
-                                                        .insert(quote_new_name.to_owned(), quote);
-                                                    response
-                                                } else {
-                                                    Some(format!(
-                                                        "No quote with name {} found",
-                                                        quote_name
-                                                    ))
-                                                };
-                                            bot.config.save().unwrap();
-                                            return response;
-                                        }
-                                        None
-                                    }),
-                                )],
-                            }],
-                        }],
-                    },
-                    CommandNode::Argument {
-                        argument_type: ArgumentType::Word,
-                        child_nodes: vec![CommandNode::final_node(
-                            true,
-                            AuthorityLevel::Viewer as usize,
-                            Arc::new(|bot, _, mut args| {
-                                let quote_name = args.remove(0);
-                                if let Some(quote) = bot.config.quotes.get(&quote_name) {
-                                    let response = Some(quote.clone());
-                                    return response;
-                                }
-                                None
-                            }),
-                        )],
-                    },
-                ],
-            }],
+            commands: vec![CommandBuilder::new()
+                .literal(["!quote"])
+                .split([random, new, remove, edit, rename, get])],
         }
     }
 }
